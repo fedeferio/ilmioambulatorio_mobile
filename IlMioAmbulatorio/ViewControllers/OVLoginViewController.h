87760b98329939d60d5c5a8145777d360066cf11
Marco Cappellini <cappellini@openview.it>
2013-01-25 17:22:38 +0100

    LoginView added

---
+++ IlMioAmbulatorio/ViewControllers/OVLoginViewController.h
@@ -0,0 +1,16 @@
+//
+//  OVLoginViewController.h
+//  IlMioAmbulatorio
+//
+//  Created by Develop on 25/01/13.
+//  Copyright (c) 2013 Openview. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OVLoginViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITextField *textUser;
+@property (weak, nonatomic) IBOutlet UITextField *textPassword;
+@property (weak, nonatomic) IBOutlet UIButton *buttonLogin;
+
+@end
---
+++ IlMioAmbulatorio/ViewControllers/OVLoginViewController.m
@@ -0,0 +1,58 @@
+//
+//  OVLoginViewController.m
+//  IlMioAmbulatorio
+//
+//  Created by Develop on 25/01/13.
+//  Copyright (c) 2013 Openview. All rights reserved.
+//
+
+#import "OVLoginViewController.h"
+#import "AFHTTPClient.h"
+
+@interface OVLoginViewController ()
+
+@end
+
+@implementation OVLoginViewController
+
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
+
+- (IBAction)actionLogin:(id)sender
+{
+    
+	AFHTTPClient *httpClient = [[AFHTTPClient alloc] initWithBaseURL:[NSURL URLWithString:@"http://webapp.ilmioambulatorio.it/"]];
+    //	[httpClient setDefaultHeader:@"X-CSRF-Token" value:[PMUserHandler sharedHelper].tokenCSRF];
+	
+	[[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:YES];
+    
+    NSDictionary* loginParams = @{@"username":@"Gregory.House",@"password":@"password"};
+    
+	[httpClient postPath:@""
+              parameters:loginParams
+                 success:^(AFHTTPRequestOperation *operation, id responseObject) {
+                     [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:NO];
+                     NSError* error;
+                     
+                     id result = [NSJSONSerialization JSONObjectWithData:responseObject options:NSJSONWritingPrettyPrinted error:&error];
+                     
+                     [[NSUserDefaults standardUserDefaults] setObject:@"" forKey:@"token"];
+                     
+                    
+                     
+                 }
+                 failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+                     [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:NO];
+                 }];
+}
+
+@end
